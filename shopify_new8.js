1eb23bd7c771d1b4315e6b7993b5e1b39477a7e8
akkvjoe <42866335+akkvjoe@users.noreply.github.com>
2021-08-31 15:09:05 +0800

    Update and rename shopify_new7.js to shopify_new8.js

---
+++ shopify_new8.js
@@ -0,0 +1,97 @@
+function getInitVariables(){
+  $.getJSON("https://api.ipify.org?format=json", function (data) {
+    // Setting text of element P with id gfg
+    ip_addr = data.ip;
+  });
+  browser = detectBrowser();
+}
+
+// Creating function that will tell the position of cursor
+// PageX and PageY will getting position values and show them in P
+function tellPos(p) {
+  // console.log("Position X : " + p.pageX + "<br />Position Y : " + p.pageY);
+  send_http_data({
+    url: "https://genesis-ai-test.herokuapp.com/data_post_test/",
+    data: {
+      ip_addr: ip_addr,
+      browser_type: browser,
+      mouse_x: p.pageX,
+      mouse_y: p.pageY,
+    },
+  });
+}
+
+
+function detectBrowser() {
+  if (
+    (navigator.userAgent.indexOf("Opera") ||
+      navigator.userAgent.indexOf("OPR")) != -1
+  ) {
+    return "Opera";
+  } else if (navigator.userAgent.indexOf("Chrome") != -1) {
+    return "Chrome";
+  } else if (navigator.userAgent.indexOf("Safari") != -1) {
+    return "Safari";
+  } else if (navigator.userAgent.indexOf("Firefox") != -1) {
+    return "Firefox";
+  } else if (
+    navigator.userAgent.indexOf("MSIE") != -1 ||
+    !!document.documentMode == true
+  ) {
+    return "IE"; //crap
+  } else {
+    return "Unknown";
+  }
+}
+
+function logging(log_msg, timestamp) {
+  timestamp = typeof timestamp !== "undefined" ? timestamp : "";
+  // if (_7.debug)
+  //   console.log(
+  //     "MF" + (_7.includeDebugTime ? " - " + timestamp : "") + ": " + log_msg
+  //   );
+  console.log("MF" + timestamp + ": " + log_msg);
+}
+
+function find_timestamp() {
+  return +new Date();
+}
+
+function send_http_data(payload_cfg) {
+  // if (!payload_cfg || !payload_cfg.url) return;
+  if (window.XMLHttpRequest) {
+    try {
+      var xml_http_obj = new window.XMLHttpRequest();
+      xml_http_obj.onreadystatechange = function () {
+        if (xml_http_obj.readyState !== 4 || !xml_http_obj.status) return;
+        if (payload_cfg.fn) payload_cfg.fn(xml_http_obj.response);
+      };
+      console.log(payload_cfg.url);
+      xml_http_obj.open(
+        payload_cfg.data ? "POST" : "GET",
+        payload_cfg.url,
+        true
+      );
+      xml_http_obj.setRequestHeader("Content-type", "application/json");
+      // if (payload_cfg._733) xml_http_obj.withCredentials = true;
+      xml_http_obj.send(JSON.stringify(payload_cfg.data));
+    } catch (err_obj) {
+      logging(
+        "Error in transmitCrossDomain (XMLHttpRequest): " + err_obj.message,
+        find_timestamp()
+      );
+    }
+  }
+}
+
+console.log("Thanks for using this site...");
+getInitVariables();
+addEventListener("mousemove", tellPos, false);
+// send_http_data({
+//   url: "https://genesis-ai-test.herokuapp.com//data_post_test/",
+//   data: {
+//     email: "mine",
+//     pswd: "nomine",
+//   },
+// });
+
