f96a0ad8fa08fdf108944269ef824db7d366385d
akkvjoe <42866335+akkvjoe@users.noreply.github.com>
2021-08-31 17:38:25 +0800

    Update shopify_new15.js

--- shopify_new15.js
+++ shopify_new15.js
@@ -11,7 +11,7 @@ var mouse_x = -1 ;
 var mouse_y = -1 ;
 
 function getInitVariables(){
-  if($){
+  if(typeof $ !== 'undefined'){
     $.getJSON("https://api.ipify.org?format=json", function (data) {
       // Setting text of element P with id gfg
       ip_addr = data.ip;
